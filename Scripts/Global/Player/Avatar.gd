4d91e09c0a452626f1c4afc2741c3b00ff8abeaa
davididev <davidwaggener@gmail.com>
2024-10-07 13:42:43 -0400

    Adding initial player UI before work

--- Scripts/Global/Player/Avatar.gd
+++ Scripts/Global/Player/Avatar.gd
@@ -1,12 +1,26 @@
 extends CharacterBody3D
 
 static var Health : int = 100;
-static var GameID : int = 1;
-var damageDelay : float = 0.0;
+var damageDelay : float = -1.0;
+var GlobalUI : PackedScene;
 
 
 func _enter_tree() -> void:
+	UpdateUI();
 	pass;
 
 func _process(delta: float) -> void:
 	pass;
+
+func Damage(amt : int) -> void:
+	if damageDelay <= 0.0:
+		DamageOverride(amt);
+
+func DamageOverride(amt : int):
+	Health -= amt;
+	UpdateUI();
+	
+func UpdateUI():  
+	#show health bar, gold, etc
+	pass;
+	
