733becd11a27542807f4b550de28c451f3e12bd0
davididev <davidwaggener@gmail.com>
2024-10-25 13:05:56 -0400

    Added scale Y to loading text
    Animation to the loading text

--- Scenes/Global/loadingscreen.gd
+++ Scenes/Global/loadingscreen.gd
@@ -2,15 +2,19 @@ class_name LoadingUI extends Node2D
 
 static var SceneToLoad = "";
 var resource_name;
+var scaleY = 0.001;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#get_node("CanvasLayer/Header").text = str("[center]Loading:\n", );
 	resource_name = str("res://Scenes/", SceneToLoad, ".tscn")
 	ResourceLoader.load_threaded_request(resource_name);
+	get_node("Camera2D/CanvasGroup/RichTextLabel").scale = Vector2(1.0, scaleY);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	scaleY = move_toward(scaleY, 1.0, 2.0 * delta);
+	get_node("Camera2D/CanvasGroup/RichTextLabel").scale = Vector2(1.0, scaleY);
 	var progress = [];
 	var status = ResourceLoader.load_threaded_get_status(resource_name, progress);
 	if status == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
