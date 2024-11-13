a61c2ddf665f1cb95e818a47ad0c1fd25077299d
davididev <davidwaggener@gmail.com>
2024-11-12 21:02:33 -0500

    Imported Becky's model and started the game

--- Scripts/Local/scene_vars.gd
+++ Scripts/Local/scene_vars.gd
@@ -1,12 +1,15 @@
 class_name SceneVars extends Node3D
 
 @export var MusicForScene = "Techno Dungeon.mp3";
+@export var DialogueOnStart : DialogueGrid;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().create_timer(0.1).timeout;
 	PlayMusic.PlaySong(MusicForScene);
-
+	if DialogueOnStart != null:
+		await get_tree().create_timer(0.1).timeout;
+		DialogueHandler.Instance.StartDialogue(DialogueOnStart);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
