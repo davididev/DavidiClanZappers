c53a9ad0e6b4c9b1b340b18edaeb1cace254d865
davididev <davidwaggener@gmail.com>
2024-11-19 13:01:37 -0500

    Started making tower

--- Scripts/Global/Interactable/TouchTeleport.gd
+++ Scripts/Global/Interactable/TouchTeleport.gd
@@ -4,18 +4,24 @@ class_name TouchTeleport extends Area3D
 @export var SoundOnOpen : String;
 @export var SceneToGoTo : String;
 @export var PositionToGoTo : Vector3;
+var awaitTimer = 0.05;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	awaitTimer = 0.05;
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if awaitTimer > 0.0:
+		awaitTimer -= delta;
 	pass
 
 
 func _on_body_entered(body: Node3D) -> void:
+	if awaitTimer > 0.0:
+		return;
 	if body.is_in_group("Player"):
 		if SoundOnOpen != "":
 			SoundFXPlayer.PlaySound(SoundOnOpen, get_tree(), global_position);
