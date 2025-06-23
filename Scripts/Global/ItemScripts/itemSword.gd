701d175af3052aa4bb886cf9b0c4f564234705fa
davididev <davidwaggener@gmail.com>
2025-06-22 20:45:18 -0400

    Intro and starting vars made for Hannah's game
    Also fixed sword script

--- Scripts/Global/ItemScripts/itemSword.gd
+++ Scripts/Global/ItemScripts/itemSword.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var swingSoundFX : String;
 @export var hitSoundFX : String;
 @export var BaseAttackPower = 4;
+@export var Colliders : Array[NodePath];
 var swingTimer = 0.0;
 
 signal OnDamage(Amount : int);  #Enemies will need to use this
@@ -14,11 +15,17 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func UseItem():
-	if swingSoundFX != "":
-		SoundFXPlayer.PlaySound(swingSoundFX, get_tree(), global_position);
-	swingTimer = 0.6;
-	pass;
- 
+	if swingTimer <= 0.0:
+		if swingSoundFX != "":
+			SoundFXPlayer.PlaySound(swingSoundFX, get_tree(), global_position);
+		swingTimer = 0.6;
+		DisableColliders(false);
+		pass;
+	
+func DisableColliders(stat : bool):
+	for np in Colliders:
+		var cs = get_node(np) as CollisionShape3D;
+		cs.call_deferred("set_disabled", stat);
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if swingTimer > 0.0:
@@ -26,6 +33,7 @@ func _process(delta: float) -> void:
 		swingTimer -= delta;
 	else:
 		get_node(refToOverlap).monitoring = false;
+		DisableColliders(true);
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
